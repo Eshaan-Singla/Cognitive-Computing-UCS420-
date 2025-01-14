53fcf08293d9928099247a678bfcfd4fd8e05d6f
Eshaan-Singla <esingla_be23@thapar.edu>
2025-01-14 17:53:13 +0530

    Add files via upload

---
+++ ASSIGNMENT-1/Q2.py
@@ -0,0 +1,20 @@
+#2.1
+a=2
+b=3
+print(a+b)
+s=a+b
+print(s)
+
+#2.2
+string1="Eshaan"
+string2="Singla"
+print(string1+" "+string2)
+full_name=string1+" "+string2
+print(full_name)
+
+#2.2
+name="Eshaan Singla"
+roll=102317111
+print(name+" "+str(roll))
+result=name+" "+str(roll)
+print(result)
